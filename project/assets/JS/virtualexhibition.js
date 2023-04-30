ad1ffd2fa65557d1dd5730570f58a211d5dff07e
wolf790206 <wolf790206@gmail.com>
2023-05-01 02:18:42 +0800

    update

--- project/assets/JS/virtualexhibition.js
+++ project/assets/JS/virtualexhibition.js
@@ -1,2 +1,6 @@
-const Loading = document.getElementById('loader');
-Loading.classList.add('ani');
+console.log(document.querySelector('#content .imageContainer'));
+if (wWidth < wHeight) document.querySelector('#content').classList.add('longscreen');
+
+setTimeout(() => {
+	document.getElementById('web').src = 'https://virsody.io/exhibition/nodata';
+}, 6400);
